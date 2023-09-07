0a84a44f854a0231bec7c244ad5edea2de08a51b
Maria <lourdes1984.sosa@gmail.com>
2023-09-07 02:08:07 -0300

    inputs con material ui y validaciones

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Formulario from "./Components/Formulario";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const handleSubmit = (valores) => {
+    console.log("APP", valores);
+  };
   return (
-    <>
-      <Formulario />
-    </>
+    <Container componente="section" maxWidth="xs">
+      <Typography variant="h4" component="h1" align="center">
+        Formulario de Registro
+      </Typography>
+
+      <Formulario handleSubmit={handleSubmit} />
+    </Container>
   );
 }
 
--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,23 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 
-export default function Formulario() {
+export default function Formulario({ handleSubmit }) {
+  const [name, setName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [novedades, setNovedades] = useState(true);
+  const [promociones, setPromociones] = useState(false);
+  const [errores, setErrores] = useState({
+    name: {
+      error: false,
+      mensaje: "Debe contener al menos 3 caracteres",
+    },
+  });
+
+  function validarNombre(nombre) {
+    if (nombre.length >= 3) {
+      return {
+        name: {
+          error: false,
+          mensaje: "",
+        },
+      };
+    } else {
+      return {
+        name: {
+          error: true,
+          mensaje: "Debe contener al menos 3 caracteres",
+        },
+      };
+    }
+  }
+
   return (
     <>
-      <h1>Formulario de Registro</h1>
-      <form action="">
-        <label htmlFor="">Nombre</label>
-        <input type="text" />
-        <label htmlFor="">Apellido</label>
-        <input type="text" />
-        <label htmlFor="">Email</label>
-        <input type="email" />
-        <label htmlFor="">Promociones</label>
-        <input type="checkbox" />
-        <label htmlFor="">Novedades</label>
-        <input type="checkbox" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit({
+            name,
+            lastName,
+            email,
+            novedades,
+            promociones,
+          });
+        }}
+      >
+        <TextField
+          id="name"
+          label="Nombre"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          error={errores.name.error}
+          helperText={errores.name.error ? errores.name.mensaje : ""}
+          onBlur={(e) => {
+            setErrores(validarNombre(e.target.value));
+          }}
+        />
 
-        <Button variant="contained">Registrarse</Button>
+        <TextField
+          id="lastName"
+          label="Apellido"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={lastName}
+          onChange={(e) => {
+            setlastName(e.target.value);
+          }}
+        />
+
+        <TextField
+          id="email"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={promociones}
+                onChange={(e) => setPromociones(e.target.checked)}
+              />
+            }
+            label="Promociones"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={novedades}
+                onChange={(e) => setNovedades(e.target.checked)}
+              />
+            }
+            label="Novedades"
+          />
+        </FormGroup>
+
+        <Button variant="contained" type="submit">
+          Registrarse
+        </Button>
       </form>
     </>
   );
